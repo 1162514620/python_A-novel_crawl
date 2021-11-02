c85e68c5d4f84fcf2e8c7d04f110620222e3fd5b
MSHE <1162514620@qq.com>
2021-11-02 11:33:10 +0800

    V11

--- 小说爬取器/小说爬取器.py
+++ 小说爬取器/小说爬取器.py
@@ -1,4 +1,6 @@
 import requests
+import httpx
+import asyncio
 import bs4
 import re
 import tkinter as tk
@@ -21,13 +23,18 @@ def find_next(res):
     N = soup.find("a", class_="next")
     return "https://www.biqugeu.net"+N.attrs['href']
 
-#获取当前页内容
+#获取当前页章节名和正文内容
 def Access_content(res):
 
     #以html的方式进行解析获取到的网页内容
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
     content=[]
+    N = soup.find_all("div", class_="bookname")
+    #将获取到的章节名写到content中
+    for each in N:
+        content .append(each.h1.text)
+        print("正在爬取  %s  的内容\n"%each.h1.text)
     #得到小说内容的TXT文本部分
     T = soup.find_all("div", id="content")
     #将获取到的内容写到content中
@@ -36,6 +43,16 @@ def Access_content(res):
 
     return content
 
+#获取文件夹路径
+def Access_directory():
+
+    #打开绘图部件
+    root = tk.Tk()
+    #隐藏绘图部件
+    root.withdraw()
+    #获得选择好的文件夹
+    return filedialog.askdirectory()
+
 def main():
 
     result = []
@@ -43,14 +60,9 @@ def main():
     host = input("请输入第一章的网址：")
     #获得小说名称
     novel_name = input("小说名称为：")
-
-    #打开绘图部件
-    root = tk.Tk()
-    #隐藏绘图部件
-    root.withdraw()
-    #获得选择好的文件夹
-    Folderpath = filedialog.askdirectory() 
-    i=0
+    #获取文件夹路径
+    Folderpath = Access_directory()
+    
     while (1):
         try:
             #打开网页
@@ -59,18 +71,14 @@ def main():
             result.extend(Access_content(res))
             #获得下一章的网址
             host = find_next(res)
-            i=i+1
-            print("第%d章\n"%i)
         except:
             break
 
-    
     #写入TXT文件
     with open(Folderpath+'\\'+novel_name+'.txt', "w", encoding="utf-8") as f:
         for each in result:
             f.write(each)
 
-   
 if __name__ == "__main__":
     main()
 
